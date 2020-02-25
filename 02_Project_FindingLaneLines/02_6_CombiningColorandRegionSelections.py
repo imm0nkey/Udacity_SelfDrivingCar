4ee15b622a2c3773e4b51ed2152da4d4f7c28ac6
imm0nkey <yidingyidingli@gmail.com>
2020-02-26 02:51:18 +0800

    20200226

---
+++ 02_Project_FindingLaneLines/02_6_CombiningColorandRegionSelections.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image
+image = mpimg.imread('test.jpg')
+
+# Grab the x and y sizes and make two copies of the image
+# 
