c7c29cd78f6f3c7e51580c6cd5e59672f74e87bd
imm0nkey <yidingyidingli@gmail.com>
2020-05-21 15:42:12 +0800

    Additional HOG features

--- 16_Project_VehicleDetectionandTracking/16_20_scikitimageHOG_get_hog.py
+++ 16_Project_VehicleDetectionandTracking/16_20_scikitimageHOG_get_hog.py
@@ -4,3 +4,19 @@ import numpy as np
 import cv2
 import glob
 from skimage.feature import hog
+
+# Read in our vehicles and non-vehicles
+images = glob.glob('*vehicles_smallset/*/*.jpeg')
+cars = []
+notcars = []
+
+for image in images:
+    if 'image' in image or 'extra' in image:
+        notcars.append(image)
+    else:
+        cars.append(image)
+
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        # Use 
