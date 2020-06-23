7def09b5b02663c30080e38bff725c4161bbe629
imm0nkey <yidingyidingli@gmail.com>
2020-06-23 10:45:52 +0800

    20200623 11.png

--- 16_Project_VehicleDetectionandTracking/16_20_scikitimageHOG_get_hog.py
+++ 16_Project_VehicleDetectionandTracking/16_20_scikitimageHOG_get_hog.py
@@ -16,7 +16,40 @@ for image in images:
     else:
         cars.append(image)
 
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
     if vis == True:
-        # Use 
+        # Use skimage.hog() to get both features and a visualization
+        features, hog_image = hog(img, orient, (pix_per_cell, pix_per_cell), (cell_per_block, cell_per_block),
+                                  block_norm='L1', visualize=True, feature_vector=feature_vec)
+        return features, hog_image
+    else:
+        # Use skimage.hog() to get features only
+        features = hog(img, orient, (pix_per_cell, pix_per_cell), (cell_per_block, cell_per_block),
+                       block_norm='L1', visualize=False, feature_vector=feature_vec)
+        return features
+
+
+# Generate a random index to look at a car image
+ind = np.random.randint(0, len(cars))
+# Read in the image
+image = mpimg.imread(cars[ind])
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# Define HOG parameters
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+# Call our function with vis=True to see an image output
+features, hog_image = get_hog_features(gray, orient, pix_per_cell, cell_per_block,
+                                       vis=True, feature_vec=False)
+
+# Plot the examples
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(image, cmap='gray')
+plt.title('Example Car Image')
+plt.subplot(122)
+plt.imshow(hog_image, cmap='gray')
+plt.title('HOG Visualization')
+plt.show()
