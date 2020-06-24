d2685fafb9c7af6c8abdd71a4401dd7abac06e4f
imm0nkey <yidingyidingli@gmail.com>
2020-06-24 16:44:24 +0800

    20200624 13_MachineLearningandStanley

---
+++ 14_SupportVectorMachines/14_11_CodingUptheSVM.py
@@ -0,0 +1,8 @@
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
